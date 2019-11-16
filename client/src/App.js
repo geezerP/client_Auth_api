2c6de00fb21c0953eec8223a80f912de1ce6da0e
geezerP <mwaurameshack64@gmail.com>
2019-11-16 09:43:15 +0300

    refactor client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Layout from './core/Layout';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Layout>
+            <div className="col-md-6 offset-md-3 text-center">
+                <h1 className="p-5">React Node MongoDB Authentication Boilerplate</h1>
+                <h2>MERN STACK</h2>
+                <hr />
+                <p className="lead">
+                    MERN stack login register system with account activation, forgot password, reset password, login
+                    with facebook and google as well as private and protected routes for authenticated user and users
+                    with the role of admin.
+                </p>
+                <h4 className="p2">Bonus</h4>
+                <p className="lead">Profile update & deployment to digital ocean cloud servers</p>
+            </div>
+        </Layout>
     );
-  }
-}
+};
 
 export default App;
